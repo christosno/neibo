bb715ced682f78b6720ca20355a19ad613b272e2
Christos Nounis <christosno1984@gmail.com>
2025-12-15 09:30:35 +0200

    add axios and tanstack query

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,18 +2,22 @@ import React from "react";
 import { Stack } from "expo-router";
 import { useAuth } from "@/authentication/useAuth";
 import { useRequestLocationPermissions } from "@/hooks";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function Layout() {
+  const queryClient = new QueryClient();
   const user = useAuth((state) => state.user);
   useRequestLocationPermissions();
   return (
-    <Stack>
-      <Stack.Protected guard={!!user}>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack.Protected>
-      <Stack.Protected guard={!user}>
-        <Stack.Screen name="login" options={{ headerShown: false }} />
-      </Stack.Protected>
-    </Stack>
+    <QueryClientProvider client={queryClient}>
+      <Stack>
+        <Stack.Protected guard={!!user}>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack.Protected>
+        <Stack.Protected guard={!user}>
+          <Stack.Screen name="login" options={{ headerShown: false }} />
+        </Stack.Protected>
+      </Stack>
+    </QueryClientProvider>
   );
 }
